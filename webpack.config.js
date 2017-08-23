95c987f5795086b15322848a3b71593ba19248c0
Ralph Dugue <rdugue1@gmail.com>
2017-08-23 16:53:24 -0400

    More tuning config still

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
+      exclude: /node_modules/,
+      include: /node_modules\/rc\/index.js/,
       loaders: ['shebang-loader', 'babel-loader'],
       include: __dirname
     }]
