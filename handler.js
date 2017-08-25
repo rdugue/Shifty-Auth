5eb20670295935d1bd00e592a6d9e1791435cd76
Ralph Dugue <rdugue1@gmail.com>
2017-08-25 07:44:43 -0400

    Fixed several logic errors

--- handler.js
+++ handler.js
@@ -5,100 +5,93 @@ import { DynamoHelper } from './dynamo-helper'
 
 const response = {
   statusCode: 200,
-  body: JSON.stringify({
-    message: 'Go Serverless v1.0! Your function executed successfully!',
-  }),
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true,
-    'Content-Type': 'application/json'
-  }
+    'Access-Control-Allow-Credentials': true
+  },
+  body: JSON.stringify({
+    message: 'Go Serverless v1.0! Your function executed successfully!',
+  })
 };
 const JWT_SECRET = process.env.JWT_SECRET
-const JWT_ALGORITHM = [process.env.JWT_ALGORITHM]
+const JWT_ALGORITHM = process.env.JWT_ALGORITHM
 const helper = new DynamoHelper()
 
 module.exports.register = (event, context, callback) => {
   if (event.body) {
     let user = JSON.parse(event.body)
-  } else {
-    response.statusCode = 400
-    response.body = JSON.stringify({ error: 'No POST body.' })
-    callback(response)
-  }
-  bcrypt.genSalt(10, (error, salt) => {
-    if (error) {
-      response.statusCode = 400
-      response.body =  JSON.stringify({ error: error })
-      callback(response)
-    } else {
-      bcrypt.hash(user.password, salt, (err, hash) => {
-        if (err) {
-          response.statusCode = 400
-          response.body =  JSON.stringify({ error: err })
-          callback(response)
-        } else {
-          user.password = hash
-          helper.createUser(user)
-          .then(item => {
-            const tokenData = {
-              userId: item.userId,
-              position: item.position,
-              company: item.company
-            }
-            response.body = JSON.stringify({
-              data: item,
-              token: jwt.sign(tokenData, JWT_SECRET, JWT_ALGORITHM)
+    bcrypt.genSalt(10, (error, salt) => {
+      if (error) {
+        callback(new Error(`salting error - ${error}`))
+      } else {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          if (err) {
+            callback(new Error(`hashing error - ${err}`))
+          } else {
+            user.password = hash
+            helper.createUser(user)
+            .then(item => {
+              const tokenData = {
+                userId: item.Attributes.userId,
+                position: item.Attributes.position,
+                company: item.Attributes.company
+              }
+              jwt.sign(tokenData, JWT_SECRET, { algorithm: JWT_ALGORITHM }, (error, token) => {
+                if (error) {
+                  callback(new Error(`token signing error - ${error}`))
+                } 
+                response.body = JSON.stringify({
+                  data: item.Attributes,
+                  token: token
+                })
+                callback(null, response)
+              })
             })
-            callback(null, response)
-          })
-          .catch(reason => {
-            response.statusCode = 400
-            response.body = JSON.stringify({ error: reason })
-            callback(response)
-          })
-        }
-      })
-    }
-  })
+            .catch(reason => callback(new Error(reason)))
+          }
+        })
+      }
+    })
+  } else {
+    callback(new Error('No POST body.'))
+  }
 };
 
 module.exports.login = (event, context, callback) => {
   if (event.body) {
     let user = JSON.parse(event.body)
-  } else {
-    response.statusCode = 400
-    response.body = JSON.stringify({ error: 'No POST body.' })
-    callback(response)
-  }
-  helper.getUser(user.userId, user.company)
-  .then(item => {
-    return bcrypt.compare(user.password, item.Item.password)
-  })
-  .catch(reason => reject(reason))
-  .then(valid => {
-    if (valid) {
-      const tokenData = {
-        userId: item.userId,
-        position: item.position,
-        company: item.company
+    let db_response = {}
+    helper.getUser(user.userId, user.company)
+    .then(item => {
+      db_response = item.Item
+      return bcrypt.compare(user.password, item.Item.password)
+    })
+    .catch(reason => reject(reason))
+    .then(valid => {
+      if (valid) {
+        const tokenData = {
+          userId: db_response.userId,
+          position: db_response.position,
+          company: db_response.company
+        }
+        jwt.sign(tokenData, JWT_SECRET, { algorithm: JWT_ALGORITHM }, (error, token) => {
+          if (error) {
+            callback(new Error(`token signing error - ${error}`))
+          } 
+          response.body = JSON.stringify({
+            data: db_response,
+            token: token
+          })
+          callback(null, response)
+        })
+      } else {
+        callback(new Error('Invalid credentials'))
       }
-      response.body = JSON.stringify({
-        data: item,
-        token: jwt.sign(tokenData, JWT_SECRET, JWT_ALGORITHM)
-      })
-      callback(null, response)
-    } else {
-      response.statusCode = 300
-      response.body = JSON.stringify({ error: 'Invalid credentials'})
-      callback(response)
-    }
-  })
-  .catch(reason => {
-    response.statusCode = 400
-    response.body = JSON.stringify({ error: reason })
-    callback(response)
-  })
+    })
+    .catch(reason => callback(new Error(reason)))
+  } else {
+    callback(new Error('No POST body.'))
+  }
 }
 
 module.exports.authorizer = (event, context, callback) => {
